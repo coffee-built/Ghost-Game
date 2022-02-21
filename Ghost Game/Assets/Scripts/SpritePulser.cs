b3891ce12e1f741541f34053790a9e15f77e9059
Abhishek Mukherjee <abhishek.mukherjee742@gmail.com>
2022-02-21 11:29:29 -0500

    Adjusted speed of ghost, and added ability of ghost to ignore walls with HumanWalls tag

--- Ghost Game/Assets/Scripts/SpritePulser.cs
+++ Ghost Game/Assets/Scripts/SpritePulser.cs
@@ -5,7 +5,6 @@ public class SpritePulser : MonoBehaviour
     public float beats_per_minute = 120;
     public float percentage_change = 2;
     public float pulses_per_beat = 1.0f;
-    public Animation anim;
 
     private float z;
 
@@ -24,7 +23,6 @@ public class SpritePulser : MonoBehaviour
     private float heightChangePerSecond;
     private float widthChangePerSecond;
 
-    private bool isGrowing = true;
     private float lastTimestamp;
 
     // Start is called before the first frame update
@@ -46,16 +44,14 @@ public class SpritePulser : MonoBehaviour
         currentWidth = initialWidth;
 
         var beats_per_second = beats_per_minute / (60.0f);
-        heightChangePerSecond = 2.0f * pulses_per_beat * (maxHeight - minHeight) * beats_per_second;
-        widthChangePerSecond = 2.0f * pulses_per_beat * (maxWidth - minWidth) * beats_per_second;
-
         lastTimestamp = Time.realtimeSinceStartup;
     }
 
     void Update()
     {
-        currentHeight = (initialHeight * percentage_change / 100) * Mathf.Sin((2 * Mathf.PI * beats_per_minute / 60.0f) * Time.realtimeSinceStartup) + initialHeight;
-        currentWidth = ((maxWidth - minWidth) / 2) * Mathf.Sin((2 * Mathf.PI * beats_per_minute / 60.0f) * Time.realtimeSinceStartup) + initialWidth;
+        var sinValue = Mathf.Sin((2 * Mathf.PI * beats_per_minute / 60.0f) * Time.realtimeSinceStartup);
+        currentHeight = (initialHeight * percentage_change / 100) * sinValue + initialHeight;
+        currentWidth = ((maxWidth - minWidth) / 2) * sinValue + initialWidth;
         
         transform.localScale = new Vector3(currentHeight, currentWidth, z);
     }
