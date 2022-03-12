7c8da0b27de09fc9bbe40ff63f3d3fac4f988649
Abhishek Mukherjee <abhishek.mukherjee742@gmail.com>
2022-03-12 15:07:38 -0500

    Adjusted rng character gen parameters to handle sprites

--- Ghost Game/Assets/Scripts/RandomCharacterBuilder.cs
+++ Ghost Game/Assets/Scripts/RandomCharacterBuilder.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class RandomCharacterBuilder : MonoBehaviour
 {
     public int charsToGenerate;
-    public int hairVerticalOffset;
-    public int headVerticalOffset;
+    public float hairVerticalOffset;
+    public float shirtVerticalOffset;
 
     public List<Sprite> hairAnimations;
-    public List<Sprite> headAnimations;
+    public List<Sprite> shirtAnimations;
     public List<Sprite> torsoAnimations;
 
 
@@ -21,30 +21,31 @@ public class RandomCharacterBuilder : MonoBehaviour
         for (int i = 0; i < charsToGenerate; i++)
         {
             var newCharHair = hairAnimations[r.Next(0, hairAnimations.Count)];
-            var newCharHead = headAnimations[r.Next(0, headAnimations.Count)];
+            var newCharShirt = shirtAnimations[r.Next(0, shirtAnimations.Count)];
             var newCharTorso = torsoAnimations[r.Next(0, torsoAnimations.Count)];
 
             var newGameObject = Instantiate(new GameObject());
             newGameObject.name = "Character #" + (i + 1);
+            newGameObject.transform.position = new Vector3(newGameObject.transform.position.x + 2*i, newGameObject.transform.position.y, newGameObject.transform.position.z);
 
             var newHairGameObject = Instantiate(new GameObject());
-            newHairGameObject.name = "Character #" + (i + 1) + "Hair";
+            newHairGameObject.name = "Character #" + (i + 1) + " Hair";
             newHairGameObject.transform.SetParent(newGameObject.transform);
             newHairGameObject.transform.position = new Vector3(newGameObject.transform.position.x, newGameObject.transform.position.y + hairVerticalOffset, newGameObject.transform.position.z); 
             var hairSpriteRenderer = newHairGameObject.AddComponent<SpriteRenderer>();
             hairSpriteRenderer.sprite = newCharHair;
             hairSpriteRenderer.sortingOrder = 5;
 
-            var newHeadGameObject = Instantiate(new GameObject());
-            newHeadGameObject.name = "Character #" + (i + 1) + "Head";
-            newHeadGameObject.transform.SetParent(newGameObject.transform);
-            newHeadGameObject.transform.position = new Vector3(newGameObject.transform.position.x, newGameObject.transform.position.y + headVerticalOffset, newGameObject.transform.position.z);
-            var headSpriteRenderer = newHeadGameObject.AddComponent<SpriteRenderer>();
-            headSpriteRenderer.sprite = newCharHead;
-            headSpriteRenderer.sortingOrder = 4;
+            var newShirtGameObject = Instantiate(new GameObject());
+            newShirtGameObject.name = "Character #" + (i + 1) + " Shirt";
+            newShirtGameObject.transform.SetParent(newGameObject.transform);
+            newShirtGameObject.transform.position = new Vector3(newGameObject.transform.position.x, newGameObject.transform.position.y + shirtVerticalOffset, newGameObject.transform.position.z);
+            var headSpriteRenderer = newShirtGameObject.AddComponent<SpriteRenderer>();
+            headSpriteRenderer.sprite = newCharShirt;
+            headSpriteRenderer.sortingOrder = 5;
 
             var newTorsoGameObject = Instantiate(new GameObject());
-            newTorsoGameObject.name = "Character #" + (i + 1) + "Torso";
+            newTorsoGameObject.name = "Character #" + (i + 1) + " Torso";
             newTorsoGameObject.transform.SetParent(newGameObject.transform);
             newTorsoGameObject.transform.position = new Vector3(newGameObject.transform.position.x, newGameObject.transform.position.y, newGameObject.transform.position.z);
             var torsoSpriteRenderer = newTorsoGameObject.AddComponent<SpriteRenderer>();
