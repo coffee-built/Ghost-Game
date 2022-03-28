46de57d06798f7b879990be7fc4d5268cbef5219
Ash Peng <ash.peng@outlook.com>
2022-03-27 18:33:49 -0700

    Added RigidBody 2D and NPC movement

--- Ghost Game/Assets/NPC_Movement.cs
+++ Ghost Game/Assets/NPC_Movement.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class NPC_Movement : MonoBehaviour
 {
+    private Rigidbody2D rb;
+
+    public int minNPCSpeed;
+    public int maxNPCSpeed;
+
+    private Vector2 direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Initial setting of variables would be done here
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;        
+
+        System.Random r = new System.Random();
+        // Movement code would be put here
+        System.Random randX = r.Next(-1,2);
+        System.Random randY = r.Next(-1,2);
+
+        Vector2 move = new Vector2(randX, randY);
+        direction = move;
+        rb.velocity = move * minNPCSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = direction * minNPCSpeed;
     }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // should we use tags for walls??
+        if(collision.gameObject.tag == "InternalWalls" || collision.gameObject.tag == "ExternalWalls")
+        {
+            // walk the other way
+            Debug.Log("player collided with wall!");
+            direction = -1 * direction;
+        }
+    }
+
 }
