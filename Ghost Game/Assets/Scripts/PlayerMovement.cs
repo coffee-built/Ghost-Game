8c846473fb030edc5a3ac6aca658e0d95857adf1
Abhishek Mukherjee <abhishek.mukherjee742@gmail.com>
2022-03-31 21:19:01 -0400

    Streamlined animations and added support for flipping sprite animations to player

--- Ghost Game/Assets/Scripts/PlayerMovement.cs
+++ Ghost Game/Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,8 @@ public class PlayerMovement : MonoBehaviour
     public float playerSpeed = 2.0f;
     private Rigidbody2D rb;
 
+    bool currentlyFacingRight = true;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,6 +17,8 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 move = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         rb.velocity = move * playerSpeed;
+
+        if ((move[0] > 0 && !currentlyFacingRight) || (move[0] < 0 && currentlyFacingRight)) Flip();
     }
 
     public void LightAbility()
@@ -25,6 +29,21 @@ public class PlayerMovement : MonoBehaviour
     public void ScreamAbility()
     {
         Debug.Log("Scream");
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = rb.transform.localScale;
+        currentScale.x *= -1;
+        rb.transform.localScale = currentScale;
+        currentlyFacingRight = !currentlyFacingRight;
+
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Vector3 currentChildScale = transform.GetChild(i).localScale;
+            currentChildScale.x *= -1;
+            transform.GetChild(i).localScale = currentChildScale;
+        }
     }
 
 }
