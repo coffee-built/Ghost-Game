8cbdb634d26ca7204a3bdba850e227f8df1eeac5
Abhishek Mukherjee <abhishek.mukherjee742@gmail.com>
2022-04-18 16:58:13 -0400

    Initial Fear Bar Fill Code

---
+++ Ghost Game/Assets/Scripts/FearBar.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FearBar : MonoBehaviour
+{
+    public int maximum;
+    public int current;
+    public Image mask;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetCurrentFill();
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+    }
+}
