759f6c33a67af19ef97a98807046bf77b2e86df6
Abhishek Mukherjee <abhishek.mukherjee742@gmail.com>
2022-04-23 13:46:51 -0400

    UI Fear Bar Improvements

--- Ghost Game/Assets/Scripts/FearBar.cs
+++ Ghost Game/Assets/Scripts/FearBar.cs
@@ -7,23 +7,45 @@ public class FearBar : MonoBehaviour
 {
     public int maximum;
     public int current;
+    public int minThresholdToAppear;
+    public int maxEndingXValue;
     public Image mask;
+    public GameObject fearBarBeginning;
+    public GameObject fearBarEnding;
+
+    private Vector3 endBarInitialPosition;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        endBarInitialPosition = fearBarEnding.transform.localPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetCurrentFill();
+        if (current > minThresholdToAppear) GetCurrentFill(current);
+        else
+        {
+            GetCurrentFill(0);
+        }
     }
 
-    void GetCurrentFill()
+    void GetCurrentFill(int fillValue)
     {
-        float fillAmount = (float)current / (float)maximum;
-        mask.fillAmount = fillAmount;
+        float fillAmount = (float)fillValue / (float)maximum;
+
+        if (fillAmount * 100 > minThresholdToAppear)
+        {
+            mask.fillAmount = fillAmount;
+
+            float movementAmount = Mathf.Min(1, fillAmount);
+            fearBarEnding.transform.localPosition = endBarInitialPosition + new Vector3((float)maxEndingXValue * (float)movementAmount, 0, 0);
+        }
+        else
+        {
+            mask.fillAmount = 0;
+            fearBarEnding.transform.localPosition = endBarInitialPosition;
+        }
     }
 }
